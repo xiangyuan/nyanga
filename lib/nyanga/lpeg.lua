70d3810ad8ea0762fd9ea7076e7c82655b3286f2
Richard Hundt <richardhundt@gmail.com>
2014-02-11 17:52:47 +0100

    remove obsolete lib directory and contents

--- lib/nyanga/lpeg.lua
+++
@@ -1,6 +0,0 @@
-local pckg = assert(package.searchpath("nyanga", package.cpath))
-local lpeg = assert(package.loadlib(pckg, "luaopen_lpeg"))()
-package.loaded["lpeg"]	      = lpeg
-package.loaded["nyanga.lpeg"] = lpeg
-return lpeg
-
